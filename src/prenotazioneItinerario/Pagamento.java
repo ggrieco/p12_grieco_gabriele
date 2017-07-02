47a0340a93f12e399d7a40af483a65e84e181b7a
ggrieco <s3508442@studenti.unige.it>
2017-07-03 00:23:33 +0200

    Add files via upload

---
+++ src/prenotazioneItinerario/Pagamento.java
@@ -0,0 +1,15 @@
+/*
+ * Class Pagamento
+ * 
+ * Version 1.0
+ */
+package prenotazioneItinerario;
+
+/**
+ * E' la classe astratta per la gestione dei dati di pagamento
+ * 
+ * @author Gabriele Grieco
+ */
+public abstract class Pagamento {
+	protected String indirizzoEmail;
+}
